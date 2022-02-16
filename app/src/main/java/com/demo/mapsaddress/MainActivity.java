29b4d4aea5e3eca2bd0d7604a81e1fd39b437b74
shahidpkd <shahidsiddiqui.pkd@gmail.com>
2022-02-15 23:47:07 -0800

    FIRST COMMIT

--- app/src/main/java/com/demo/mapsaddress/MainActivity.java
+++ app/src/main/java/com/demo/mapsaddress/MainActivity.java
@@ -3,18 +3,24 @@ package com.demo.mapsaddress;
 import static androidx.constraintlayout.motion.widget.Debug.getLocation;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +28,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.demo.mapsaddress.model.SendLocation;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -33,13 +40,19 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
-    Button btLocation, openMap;
+    Button btLocation, openMap, sendLocation;
     TextView textView1, textView2, textView3, textView4, textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
+    String latitude, longitude, address, deviceData;
+    String imei = "";
     Handler handler = new Handler();
     Runnable runnable;
-    int delay = 10000;
+    int delay = 1000000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +62,24 @@ public class MainActivity extends AppCompatActivity {
 
         btLocation = findViewById(R.id.bt_location);
         openMap = findViewById(R.id.openMap);
+        sendLocation = findViewById(R.id.locationSend);
         textView1 = findViewById(R.id.text_view1);
         textView2 = findViewById(R.id.text_view2);
         textView3 = findViewById(R.id.text_view3);
         textView4 = findViewById(R.id.text_view4);
         textView5 = findViewById(R.id.text_view5);
 
+
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        sendLocation.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                deviceData = getIMEI(MainActivity.this);
+                sendLocationToServer();
+            }
+        });
 
 
         btLocation.setOnClickListener(new View.OnClickListener() {
@@ -63,14 +87,77 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // check permissions
                 if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
                     getLocations();
                 } else {
                     ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE}, 44);
+                    Toast.makeText(MainActivity.this, "Phonbe", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public String getIMEI(Activity activity) {
+
+        TelephonyManager telephonyManager = (TelephonyManager) activity.getSystemService(MainActivity.TELEPHONY_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            imei = telephonyManager.getImei(1);
+//        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            imei = Settings.Secure.getString(
+                    getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+        } else {
+            final TelephonyManager mTelephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return imei;
+            }
+            if (mTelephony.getDeviceId() != null) {
+                imei = mTelephony.getDeviceId();
+            } else {
+                imei = Settings.Secure.getString(
+                        getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+            }
+        }
+        return imei;
+    }
+
+    private void sendLocationToServer() {
+
+        ApiInterface service = Api.getRetrofitInstance().create(ApiInterface.class);
+        Call<SendLocation> call = service.sendLocation(deviceData, latitude, longitude, address);
+        call.enqueue(new Callback<SendLocation>() {
+            @Override
+            public void onResponse(Call<SendLocation> call, Response<SendLocation> response) {
+                Toast.makeText(MainActivity.this, response.body().getMESSAGE(), Toast.LENGTH_SHORT).show();
+                Log.d("tag", deviceData+ "              "+latitude+ "            "+longitude+ "             "+address);
+            }
+
+            @Override
+            public void onFailure(Call<SendLocation> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+                Log.d("tag", t.toString());
+
+            }
+        });
+
+
+//        Toast.makeText(MainActivity.this, "call", Toast.LENGTH_SHORT).show();
 
 
     }
@@ -118,12 +205,25 @@ public class MainActivity extends AppCompatActivity {
                                 "<font color='#6200EE'><b>Address :</b><br></font>"
                                         + addresses.get(0).getAddressLine(0)
                         ));
+                        Double latitudeTemp = addresses.get(0).getLatitude();
+                        latitude = latitudeTemp.toString();
+                        Double longitudeTemp = addresses.get(0).getLongitude();
+                        longitude = longitudeTemp.toString();
+                        address = addresses.get(0).getAddressLine(0);
+
+//                        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                            deviceData = tm.getImei();
+//                        }
+
+
                         openMap.setVisibility(View.VISIBLE);
+                        sendLocation.setVisibility(View.VISIBLE);
                         openMap.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
                                 String strUri = "http://maps.google.com/maps?q=loc:" + addresses.get(0).getLatitude() + "," + addresses.get(0).getLongitude() + " (" + addresses.get(0).getAddressLine(0) + ")";
-                                Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(strUri));
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(strUri));
 
                                 intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
 
@@ -148,10 +248,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         handler.postDelayed(runnable = new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             public void run() {
                 handler.postDelayed(runnable, delay);
 
                 getLocations();
+                deviceData = getIMEI(MainActivity.this);
+                sendLocationToServer();
 
                 Toast.makeText(MainActivity.this, "call Address every 10 sec",
                         Toast.LENGTH_SHORT).show();
